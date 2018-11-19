d3d2b6a47e417d866beb65f3292100d6ee446da8
Kaizhe Huang <derek0405@gmail.com>
2018-11-19 11:00:03 -0800

    v2

---
+++ pkg/webhook/default_server/pods/anchore/client.go
@@ -0,0 +1,150 @@
+package anchore
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/golang/glog"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"reflect"
+	"strings"
+	"time"
+)
+
+var (
+	transCfg = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
+	}
+
+	client = &http.Client{
+		Transport: transCfg,
+	}
+)
+
+func anchoreRequest(path string, bodyParams map[string]string, method string) ([]byte, error) {
+	username := os.Getenv("ANCHORE_CLI_USER")
+	password := os.Getenv("ANCHORE_CLI_PASS")
+	anchoreEngineURL := os.Getenv("ANCHORE_CLI_URL")
+	fullURL := anchoreEngineURL + path
+
+	bodyParamJson, err := json.Marshal(bodyParams)
+	req, err := http.NewRequest(method, fullURL, bytes.NewBuffer(bodyParamJson))
+	if err != nil {
+		glog.Fatal(err)
+	}
+	req.SetBasicAuth(username, password)
+	glog.Infof("Sending request to %s, with params %s", fullURL, bodyParams)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to complete request to Anchore: %v", err)
+	}
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	//	glog.Info("Anchore Response Body: " + string(bodyText))
+	if err != nil {
+		return nil, fmt.Errorf("failed to complete request to Anchore: %v", err)
+	}
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("response from Anchore: %d", resp.StatusCode)
+	}
+	return bodyText, nil
+}
+
+func getStatus(digest string, tag string) bool {
+	path := fmt.Sprintf("/images/%s/check?tag=%s&history=false&detail=false", digest, tag)
+	body, err := anchoreRequest(path, nil, "GET")
+	if err != nil {
+		glog.Error(err)
+		return false
+	}
+	fmt.Println("Anchore Response Body: " + string(body))
+	var result []map[string]map[string][]SHAResult
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		glog.Error(err)
+		return false
+	}
+
+	foundStatus := findResult(result)
+
+	// Is this the easiest way to get this info?
+	return strings.ToLower(foundStatus) == "pass"
+}
+
+func findResult(parsed_result []map[string]map[string][]SHAResult) string {
+	//Looks thru a parsed result for the status value, assumes this result is for a single image
+
+	digest := reflect.ValueOf(parsed_result[0]).MapKeys()[0].String()
+	tag := reflect.ValueOf(parsed_result[0][digest]).MapKeys()[0].String()
+	return parsed_result[0][digest][tag][0].Status
+}
+
+func getImage(imageRef string) (Image, error) {
+	// Tag or repo??
+	params := map[string]string{
+		"tag":     imageRef,
+		"history": "true",
+	}
+
+	body, err := anchoreRequest("/images", params, "GET")
+	if err != nil {
+		glog.Error(err)
+		return Image{}, err
+	}
+
+	var images []Image
+	err = json.Unmarshal(body, &images)
+
+	if err != nil {
+		return Image{}, fmt.Errorf("failed to unmarshal JSON from response: %v", err)
+	}
+
+	return images[0], nil
+}
+func getImageDigest(imageRef string) (string, error) {
+	image, err := getImage(imageRef)
+	if err != nil {
+		return "", err
+	}
+	return image.ImageDigest, nil
+}
+
+func waitForImageLoaded(image string) (digest string, err error) {
+	err = addImage(image)
+	if err != nil {
+		return
+	}
+	count := 0
+	digest, err = getImageDigest(image)
+	for err != nil && count < 30 {
+		digest, err = getImageDigest(image)
+		time.Sleep(time.Second)
+		count++
+	}
+	return
+}
+
+func addImage(image string) error {
+	params := map[string]string{"tag": image}
+	_, err := anchoreRequest("/images", params, "POST")
+	if err != nil {
+		return err
+	}
+	glog.Infof("Added image to Anchore Engine: %s", image)
+	return nil
+}
+
+func CheckImage(image string) bool {
+	digest, err := waitForImageLoaded(image)
+	if err != nil {
+		glog.Error(err)
+		return false
+	}
+	return getStatus(digest, image)
+}
---
+++ pkg/webhook/default_server/pods/validating/pods_create_handler.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2018 Kaizhe Huang.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validating
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/glog"
+	"image-scan-k8s-webhook/pkg/webhook/default_server/pods/anchore"
+	"net/http"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission/types"
+)
+
+func init() {
+	webhookName := "validating-create-pods"
+	if HandlerMap[webhookName] == nil {
+		HandlerMap[webhookName] = []admission.Handler{}
+	}
+	HandlerMap[webhookName] = append(HandlerMap[webhookName], &PodCreateHandler{})
+}
+
+// PodCreateHandler handles Pod
+type PodCreateHandler struct {
+	// Client  client.Client
+
+	// Decoder decodes objects
+	Decoder types.Decoder
+}
+
+func (h *PodCreateHandler) validatingPodFn(ctx context.Context, obj *corev1.Pod) (bool, string, error) {
+	// TODO(user): implement your admission logic
+	allowed := true
+	msg := "allowed to be admitted"
+
+	for _, container := range obj.Spec.Containers {
+		image := container.Image
+		glog.Info("Checking image: " + image)
+		if !anchore.CheckImage(image) {
+			allowed = false
+			msg = fmt.Sprintf("Image failed policy check: %s", image)
+			glog.Warning(msg)
+			return allowed, msg, nil
+		} else {
+			glog.Info("Image passed policy check: " + image)
+		}
+	}
+
+	return allowed, msg, nil
+}
+
+var _ admission.Handler = &PodCreateHandler{}
+
+// Handle handles admission requests.
+func (h *PodCreateHandler) Handle(ctx context.Context, req types.Request) types.Response {
+	obj := &corev1.Pod{}
+
+	err := h.Decoder.Decode(req, obj)
+	if err != nil {
+		return admission.ErrorResponse(http.StatusBadRequest, err)
+	}
+
+	allowed, reason, err := h.validatingPodFn(ctx, obj)
+	if err != nil {
+		return admission.ErrorResponse(http.StatusInternalServerError, err)
+	}
+	return admission.ValidationResponse(allowed, reason)
+}
+
+//var _ inject.Client = &PodCreateHandler{}
+//
+//// InjectClient injects the client into the PodCreateHandler
+//func (h *PodCreateHandler) InjectClient(c client.Client) error {
+//	h.Client = c
+//	return nil
+//}
+
+var _ inject.Decoder = &PodCreateHandler{}
+
+// InjectDecoder injects the decoder into the PodCreateHandler
+func (h *PodCreateHandler) InjectDecoder(d types.Decoder) error {
+	h.Decoder = d
+	return nil
+}
